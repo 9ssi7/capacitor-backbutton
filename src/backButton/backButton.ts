56e0d3bddaaabab37323d26180993ca62b393556
ssibrahimbas <sami.salih@windowslive.com>
2022-01-31 21:27:09 +0300

    - documentation has been improved

--- src/backButton/backButton.ts
+++ src/backButton/backButton.ts
@@ -5,17 +5,54 @@ export type NextFunction = () => void;
 export type CallbackFunction = (event: BackButtonListenerEvent, next: NextFunction) => any | Promise<any>;
 
 export interface BackButtonListener {
+    /**
+     * @description unique key backbutton listener
+     * @used Required for the unsubscribe method
+     * @since 0.0.1
+     * */
     unique: string;
+
+    /**
+     * @description Determines the priority order of this function. If you enter 99 and you have another function with the value 100, your function with the value 99 will not run until the function with the value 100 calls next function.
+     * @since 0.0.1
+     * */
     priority: number;
+
+    /**
+     * @description Your function to call when the back button event is called.
+     * @warning If your callback does not call the next function, the event will not continue to run. Call next if you want the event to go to other listeners.
+     * @since 0.0.1
+     * */
     callback: CallbackFunction;
 }
 
 export interface IBackButtonHardware {
     name: string;
+
+    /**
+     * @description required @capacitor/app plugin
+     * @since 0.0.1
+     * */
     $app: AppPlugin;
 
+    /**
+     * @description starts listening for backbutton event
+     * @since 0.0.1
+     * */
     listen: () => void;
+
+    /**
+     * @description add listener to backbutton priority queue
+     * @params {EventListener} eventListener
+     * @since 0.0.1
+     * */
     subscribe: (eventListener: BackButtonListener) => void;
+
+    /**
+     * @description remove listener from the backbutton priority queue
+     * @params {EventListener} eventListener
+     * @since 0.0.1
+     * */
     unsubscribe: (eventListener: BackButtonListener) => void;
 }
 
